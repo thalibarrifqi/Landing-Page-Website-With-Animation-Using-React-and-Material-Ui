69fcb94a9a24b4f7f3cc1fca56814eebfa69a6d5
thalibarrifqi <thalibarrifqi@gmail.com>
2020-10-14 21:10:29 +0700

    mengubah artist

--- src/static/artist.js
+++ src/static/artist.js
@@ -1,6 +1,6 @@
 const artist=[
     {
-        title: 'Share Your Playlist',
+        title: 'Share Your Favorite Playlist',
         desc: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
         imageUrl: process.env.PUBLIC_URL+'/assets/musician4.jpg',
         direction: 'left',
